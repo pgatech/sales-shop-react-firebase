de17e837778de56fd4fc68538a492a8cad67e48d
pgatech <prayogahakimm@gmail.com>
2022-12-03 15:12:14 +0700

    React App Shop in Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+// Pages Component
+import Registrasi from './pages/registrasi/index';
+import Login from './pages/login/Index';
+import LupaPassword from './pages/lupa-password/Index';
+import NotFound from './pages/404/index';
+
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/registrasi" component={Registrasi} />
+        <Route path="/login" component={Login} />
+        <Route path="/lupa-password" component={LupaPassword} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
-export default App;
+export default App
