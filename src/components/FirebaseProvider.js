14f0e7e0ecd0661e387a934a4cb49024403df611
pgatech <prayogahakimm@gmail.com>
2022-12-14 11:20:10 +0700

    Integration Auth Firebase

---
+++ src/components/FirebaseProvider.js
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+import firebaseConfig from "../config/firebase";
+import {useAuthState} from 'react-firebase-hooks/auth';
+
+firebase.initializeApp(firebaseConfig);
+
+const FirebaseContext = React.createContext();
+
+// Custom Hooks
+export function useFirebase(){
+    return useContext(FirebaseContext);
+}
+
+function FirebaseProvider(props){
+    
+    const auth = firebase.auth();
+    const firestore = firebase.firestore();
+    const storage = firebase.storage();
+
+    const [user, loading, error] = useAuthState(auth);
+
+    return <FirebaseContext.Provider value={{
+            auth, 
+            firestore, 
+            storage, 
+            user, 
+            loading,
+            error
+    }}>
+        {props.children}
+    </FirebaseContext.Provider> 
+}
+
+export default FirebaseProvider;
---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmRkfB1eKj-vKbMLUxeNpGC4Q5cBKS43A",
+    authDomain: "sales-shop-pgatech.firebaseapp.com",
+    projectId: "sales-shop-pgatech",
+    storageBucket: "sales-shop-pgatech.appspot.com",
+    messagingSenderId: "250152287108",
+    appId: "1:250152287108:web:c030983aac9f8c3394fbd1"
+  };
+
+  export default firebaseConfig;
